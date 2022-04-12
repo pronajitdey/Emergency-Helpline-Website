3ab151dad25dfcd1a0a4ec51356fcf91c337d029
Pronajit Dey <deypronajit@gmail.com>
2022-04-12 23:29:41 +0530

    Added comments in main.js file

--- main.js
+++ main.js
@@ -17,24 +17,28 @@ btn[1].addEventListener('click', () => {
   }
 });
 
+// Btn click for fire station
 btn[2].addEventListener('click', () => {
   if (confirm("Dial 101       Or call to National Emergency Helpline")) {
     location.href = '../twilioClient/twilio.html';
   }
 });
 
+// Btn click for women helpline center
 btn[3].addEventListener('click', () => {
   if (confirm("Dial 1091        Or call to National Emergency Helpline")) {
     location.href = '../twilioClient/twilio.html';
   }
 });
 
+// Btn click for railway enquiry center
 btn[4].addEventListener('click', () => {
   if (confirm("Dial 131       Or call to National Emergency Helpline")) {
     location.href = '../twilioClient/twilio.html';
   }
 });
 
+// Btn click for anti-ragging helpline center
 btn[5].addEventListener('click', () => {
   if (confirm("Dial 1800 1805 522       Or call to National Emergency Helpline")) {
     location.href = '../twilioClient/twilio.html';
